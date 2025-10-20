75faf0ba48661e33493be7b4bb58013825a23e53
= <=>
2025-10-20 10:15:58 +0100

    conference service

---
+++ confernce/src/main/java/com/zakaria/confernce/ConfernceApplication.java
@@ -0,0 +1,13 @@
+package com.zakaria.confernce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfernceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfernceApplication.class, args);
+	}
+
+}
---
+++ confernce/src/test/java/com/zakaria/confernce/ConfernceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zakaria.confernce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfernceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
