b8bbfb832e0e6cbf43654f7ccb73e41cc76953c9
= <=>
2025-10-20 09:17:11 +0100

    initial commit

---
+++ src/main/java/com/zakaria/confernce_keynote_management/ConfernceKeynoteManagementApplication.java
@@ -0,0 +1,13 @@
+package com.zakaria.confernce_keynote_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfernceKeynoteManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfernceKeynoteManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zakaria/confernce_keynote_management/ConfernceKeynoteManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zakaria.confernce_keynote_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfernceKeynoteManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
