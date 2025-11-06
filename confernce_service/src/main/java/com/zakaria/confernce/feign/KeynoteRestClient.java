5e830175a243ba77713bb64f8f4d47d30e7741b3
= <=>
2025-11-06 19:06:20 +0100

    added gateway and discovery services

--- confernce_service/src/main/java/com/zakaria/confernce/feign/KeynoteRestClient.java
+++ confernce_service/src/main/java/com/zakaria/confernce/feign/KeynoteRestClient.java
@@ -6,7 +6,7 @@ import org.springframework.hateoas.PagedModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "keynote-service", url = "http://localhost:8081")
+@FeignClient(name = "keynote-service")
 public interface KeynoteRestClient {
     @GetMapping("/api/keynotes/{id}")
     Keynote getKeynoteById(@PathVariable Long id);
